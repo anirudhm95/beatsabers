97e2172a2c11e3fe2fe4cdc983f205a94156d03f
Cheungsf <cheungsf@users.noreply.github.com>
2018-03-08 19:53:56 -0800

    Project Environment

---
+++ BeatSabers/BeatSabers/Assets/Scripts/sucess1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sucess1 : MonoBehaviour {
+
+	public KeyCode activateString;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetKeyDown (activateString)) {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 2);
+
+			StartCoroutine (retractCollider ());
+		}
+		
+	}
+
+	IEnumerator retractCollider()
+	{
+		yield return new WaitForSeconds (.75f);
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, -2);
+		yield return new WaitForSeconds (.75f);
+		GetComponent<Rigidbody> ().velocity = new Vector3 (0, 0, 0);
+	}
+}
