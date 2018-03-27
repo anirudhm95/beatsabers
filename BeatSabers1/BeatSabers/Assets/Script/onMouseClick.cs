b662722802a591b2442f22fe95d8015c775653a0
Hung Do <hung95do@yahoo.com>
2018-03-27 11:50:14 -0700

    Updated with collision but not working yet

--- BeatSabers1/BeatSabers/Assets/Script/onMouseClick.cs
+++ BeatSabers1/BeatSabers/Assets/Script/onMouseClick.cs
@@ -3,16 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class onMouseClick : MonoBehaviour {
-
+    private bool _clickable = false;
+    private Collider2D myCollider;
+    public LineRenderer myLine;
     // Use this for initialization
     void Start () {
-		
+        myCollider = gameObject.GetComponent<Collider2D>();
 	}
 
     // Update is called once per frame
-    // Update is called once per frame
     void Update()
     {
+        OnMouseDown();
+        OnTriggerEnter(myCollider);
+        //OnTriggerExit(myCollider);
+        
+
+    }
+
+
+
+    void OnTriggerEnter(Collider2D other)
+    {
+        if (other.gameObject.tag == "line")
+        {
+            Debug.Log("hit hit hit");
+            _clickable = true;
+        }
+    }
+
+
+    void OnMouseDown()
+    {
+        if (!_clickable) return;
+        //do your interaction stuff below this line
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -28,4 +52,16 @@ public class onMouseClick : MonoBehaviour {
             }
         }
     }
+
+    
+    //void OnTriggerExit(Collider2D other)
+    //{
+    //    if (other.gameObject.name != "LineRenderer")
+    //    {
+    //        Debug.Log("nope nope nope");
+    //        _clickable = false;
+    //    }
+    //}
+
+
 }
