161498ad5c3c85cbcc461f11f3e582104a329904
saengdueancalderaz <happygoldfish8@gmail.com>
2018-04-23 11:52:05 -0700

    Login UI,stillneed work on background and save and load player's data'

---
+++ BeatSabers/BeatSabers/Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+
+	public string sceneToLoad = "Login";
+
+	public PlayerProgressHolder plyerprogress;
+	
+	// Update is called once per frame
+	void Update () {
+		if (plyerprogress.GameLoaded)
+		{
+			SceneManager.LoadScene (sceneToLoad);
+		}
+		
+	}
+}
