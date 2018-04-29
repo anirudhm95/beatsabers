73f2cdd3a020b25e7e32a4f4f98c795f90e95696
Hung Do <hung95do@yahoo.com>
2018-04-29 15:50:16 -0700

    Integrated color change and reaction area scoring

--- BeatSabers/BeatSabers/Assets/Scripts/SaberCollisionRight.cs
+++ BeatSabers/BeatSabers/Assets/Scripts/SaberCollisionRight.cs
@@ -7,6 +7,8 @@ public class SaberCollisionRight : MonoBehaviour
     SteamVR_Controller.Device device { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     SteamVR_TrackedObject trackedObj;
     GameObject tracked;
+    public float distance = 0.0f;
+    public int score = 0;
     public static GameObject DifficultyManager;
     // Use this for initialization
     void Start()
@@ -24,11 +26,38 @@ public class SaberCollisionRight : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.tag == "Respawn")
+        {
+            GetComponent<ParticleSystem>().startColor = new Color(255, 255, 255, 255);
+        }
+
+        if (other.tag == "Reaction")
+        {
+            GetComponent<ParticleSystem>().startColor = new Color(0, 0, 0, 255);
+        }
         if (device.velocity.sqrMagnitude > 1)
         {
             if (other.tag == "PlayerRight")
             {
-                 DifficultyManager.GetComponent<DifficultyManager>().incrementNotesHit();
+                string hitbox = "Reaction";
+                GameObject hitBox = GameObject.FindGameObjectWithTag(hitbox);
+                distance = Mathf.Abs(transform.position.z - hitBox.transform.position.z);
+
+                if (distance < 0.2)
+                {
+                    score = 5;
+                }
+                else if (distance < 0.5)
+                {
+                    score = 2;
+                }
+                else
+                {
+                    score = 1;
+                }
+
+                Debug.Log("Score: " + score);
+                DifficultyManager.GetComponent<DifficultyManager>().incrementNotesHit();
                 Destroy(gameObject);
                 SteamVR_Controller.Input(3).TriggerHapticPulse(3999);
 
