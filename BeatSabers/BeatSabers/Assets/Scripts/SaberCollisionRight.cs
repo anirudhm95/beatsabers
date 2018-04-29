954e0db614c3cbb8e6c9baded3d9ddf5c409ec13
Anirudh Mohan <anirudhmohan@Anirudhs-MacBook-Pro.local>
2018-04-29 10:30:54 -0700

    Changed the terrain in forest and made a space objects static

---
+++ BeatSabers/BeatSabers/Assets/Scripts/SaberCollisionRight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaberCollisionRight : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "PlayerRight")
+        {
+            Destroy(gameObject);
+            SteamVR_Controller.Input(2).TriggerHapticPulse(3000);
+        }
+        Debug.Log(message: other.tag);
+    }
+}
